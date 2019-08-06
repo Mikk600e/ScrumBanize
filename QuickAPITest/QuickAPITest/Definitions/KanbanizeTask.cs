6dbc6c181788b333f5997c63fd77fcd8fa4dd6c2
Mikkel Emil Rohde Glerup <mikk600e@student.eadania.dk>
2019-08-06 15:32:58 +0200

    Fixed github merge problems

--- QuickAPITest/QuickAPITest/Definitions/KanbanizeTask.cs
+++ QuickAPITest/QuickAPITest/Definitions/KanbanizeTask.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace QuickAPITest.Definitions
 {
-    class KanbanizeTask
+    public class Item
     {
         public string TaskId { get; set; }
         public string Priority { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
 
     }
--- QuickAPITest/QuickAPITest/Program.cs
+++ QuickAPITest/QuickAPITest/Program.cs
@@ -6,39 +6,41 @@ using System.Threading.Tasks;
 using QuickAPITest.Definitions;
 using RestSharp;
 using RestSharp.Authenticators;
-
+using RestSharp.Serialization;
+using RestSharp.Deserializers;
+using System.Xml;
+using System.Xml.Linq;
 namespace QuickAPITest
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-            // List<KanbanizeTask> kanbanizeTasks = GetKanbanizeTasks("9", "til udvikler");
+			List<Item> kanbanizeTasks = GetKanbanizeTasks("9", "til udvikler");
+			//scrumwise scrumwiseConnection = new Scrumwise("niels@freeway.dk", "591D5EAF2868B8B0531D9B0BF03A017AE3F87BFFDF6A4919FC375CB6229B7351", "https://api.scrumwise.com/service/api/v1/");
+		}
 
-            Scrumwise scrumwiseConnection = new Scrumwise("niels@freeway.dk", "591D5EAF2868B8B0531D9B0BF03A017AE3F87BFFDF6A4919FC375CB6229B7351", "https://api.scrumwise.com/service/api/v1/");
-            /*ScrumwiseItem test = new ScrumwiseItem();
-            test.Title = "Test From Script";
-            test.Description = "Created from the script";
-            test.BacklogListId = "191469-2531-15";
-            test.ProjectId = "191469-0-5";
-            test.Priority = ScrumwisePriority.Normal;
-            bool succes = scrumwiseConnection.CreateBacklogItem(test);*/
+		static List<Item> GetKanbanizeTasks(string boardId, string laneName)
+		{
+			List<Item> container = new List<Item>();
+			KanbanizeTaskList kanbanizeTasks = new KanbanizeTaskList();
+			var client = new RestClient("https://freeway.kanbanize.com/index.php/api/kanbanize");
+			var request = new RestRequest("/get_all_tasks", Method.POST);
+			request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
+			request.AddJsonBody(new { boardid = boardId, lane = laneName });
+			var response = client.Post(request);
+			var xmlDeserializer = new RestSharp.Deserializers.XmlDeserializer();
+			/*
+			 * foreach result
+			 * new Kanb anizerTask = result
+			 * add task to list
+			 */
 
-        }
+			var result = xmlDeserializer.Deserialize<KanbanizeTaskList>(response);
+			container.AddRange(result.TaskList);
 
-        static List<KanbanizeTask> GetKanbanizeTasks( string boardId, string laneName)
-        {
-            List<KanbanizeTask> kanbanizeTasks = new List<KanbanizeTask>();
 
-            var client = new RestClient("https://freeway.kanbanize.com/index.php/api/kanbanize");
-            var request = new RestRequest("/get_all_tasks", Method.POST);
-            request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
-            request.AddJsonBody(new { boardid = boardId, lane = laneName });
-            var response = client.Post(request);
-
-            return kanbanizeTasks;
-        }
-
-        
+			return container;
+		}
 	}
 }
