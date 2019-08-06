455c5a1c77a0e72994e6a789850c4e1f864204e3
Mikkel Emil Rohde Glerup <mikk600e@student.eadania.dk>
2019-08-06 09:24:52 +0200

    Initial commit

---
+++ QuickAPITest/QuickAPITest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace QuickAPITest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new RestClient("https://freeway.kanbanize.com/index.php/api/kanbanize");
+			var request = new RestRequest("/get_all_tasks", Method.POST);
+			//request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
+			request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
+			request.AddJsonBody(new { boardid = "9", lane = "til udvikler" });
+			var response = client.Post(request);
+		}
+	}
+}
