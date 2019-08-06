168dc22897b561773e2b8fe09d06d58709f4bd39
Taager <taager2201@gmail.com>
2019-08-06 10:17:40 +0200

    Moved functionality and added descriptions

--- QuickAPITest/QuickAPITest/Program.cs
+++ QuickAPITest/QuickAPITest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuickAPITest.Definitions;
 using RestSharp;
 using RestSharp.Authenticators;
 
@@ -12,12 +13,23 @@ namespace QuickAPITest
 	{
 		static void Main(string[] args)
 		{
-			var client = new RestClient("https://freeway.kanbanize.com/index.php/api/kanbanize");
-			var request = new RestRequest("/get_all_tasks", Method.POST);
-			//request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
-			request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
-			request.AddJsonBody(new { boardid = "9", lane = "til udvikler" });
-			var response = client.Post(request);
+            List<KanbanizeTask> kanbanizeTasks = GetKanbanizeTasks("9", "til udvikler");
+
 		}
+
+        static List<KanbanizeTask> GetKanbanizeTasks( string boardId, string laneName)
+        {
+            List<KanbanizeTask> kanbanizeTasks = new List<KanbanizeTask>();
+
+            var client = new RestClient("https://freeway.kanbanize.com/index.php/api/kanbanize");
+            var request = new RestRequest("/get_all_tasks", Method.POST);
+            request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
+            request.AddJsonBody(new { boardid = boardId, lane = laneName });
+            var response = client.Post(request);
+
+            return kanbanizeTasks;
+        }
+
+        
 	}
 }
