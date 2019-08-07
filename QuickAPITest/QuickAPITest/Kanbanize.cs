2a4e93970c9ed9667880a0c814aa1fd1306423bd
Mikkel Emil Rohde Glerup <mikk600e@student.eadania.dk>
2019-08-07 08:37:08 +0200

    Moved GetKanbanizeTasks to it's own class.

---
+++ QuickAPITest/QuickAPITest/Kanbanize.cs
@@ -0,0 +1,40 @@
+﻿using QuickAPITest.Definitions;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickAPITest
+{
+	class Kanbanize
+	{
+		private string _boardID;
+		private string _lane;
+
+		public Kanbanize(string boardID, string lane)
+		{
+			this._boardID = boardID;
+			this._lane = lane;
+		}
+		public bool GetKanbanizeTasks(string boardId, string laneName)
+		{
+			//List<Item> container = new List<Item>();
+			KanbanizeTaskList kanbanizeTasks = new KanbanizeTaskList();
+			var client = new RestClient("https://freeway.kanbanize.com/index.php/api/kanbanize");
+			var request = new RestRequest("/get_all_tasks", Method.POST);
+			request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
+			request.AddJsonBody(new { boardid = boardId, lane = laneName });
+			var response = client.Post(request);
+			var xmlDeserializer = new RestSharp.Deserializers.XmlDeserializer();
+
+			kanbanizeTasks = xmlDeserializer.Deserialize<KanbanizeTaskList>(response);
+			//container.AddRange(result.TaskList);
+
+
+			return true;
+		}
+
+	}
+}
--- QuickAPITest/QuickAPITest/Program.cs
+++ QuickAPITest/QuickAPITest/Program.cs
@@ -16,27 +16,9 @@ namespace QuickAPITest
 	{
 		static void Main(string[] args)
 		{
-            KanbanizeTaskList kanbanizeTasks = GetKanbanizeTasks("9", "til udvikler");
+			Kanbanize kanbanizeTasks = new Kanbanize("9", "til udvikler");
 			Scrumwise scrumwiseConnection = new Scrumwise("niels@freeway.dk", "591D5EAF2868B8B0531D9B0BF03A017AE3F87BFFDF6A4919FC375CB6229B7351", "https://api.scrumwise.com/service/api/v1/");
 
         }
-
-		static KanbanizeTaskList GetKanbanizeTasks(string boardId, string laneName)
-		{
-			//List<Item> container = new List<Item>();
-			KanbanizeTaskList kanbanizeTasks = new KanbanizeTaskList();
-			var client = new RestClient("https://freeway.kanbanize.com/index.php/api/kanbanize");
-			var request = new RestRequest("/get_all_tasks", Method.POST);
-			request.AddHeader("apikey", "mMt64VOgJK4pqlSKhnE6XUCoLDCOcbAEoFUtUJjI");
-			request.AddJsonBody(new { boardid = boardId, lane = laneName });
-			var response = client.Post(request);
-			var xmlDeserializer = new RestSharp.Deserializers.XmlDeserializer();
-            
-            kanbanizeTasks = xmlDeserializer.Deserialize<KanbanizeTaskList>(response);
-			//container.AddRange(result.TaskList);
-
-
-			return kanbanizeTasks;
-		}
 	}
 }
