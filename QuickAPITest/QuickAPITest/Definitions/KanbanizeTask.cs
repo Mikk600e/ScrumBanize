43ef49c22f127edc5b80b9df485f1422c78f414a
Mikkel Emil Rohde Glerup <mikk600e@student.eadania.dk>
2019-08-07 13:40:12 +0200

    Completed ConvertKanbasToScrum();

--- QuickAPITest/QuickAPITest/Definitions/KanbanizeTask.cs
+++ QuickAPITest/QuickAPITest/Definitions/KanbanizeTask.cs
@@ -8,10 +8,14 @@ namespace QuickAPITest.Definitions
 {
     public class Item
     {
-        public string TaskId { get; set; }
-        public string Priority { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string TaskId { get; set; } //v
+		//public string ExternalId { get; set; }
+		
+        public string Priority { get; set; } //v
+        public string Title { get; set; } //v
+        public string Description { get; set; } //v
+		public string ProjectId { get; set; } //v
+		public List<Subtaskdetail> subtaskdetails { get; set; }
 
-    }
+	}
 }
